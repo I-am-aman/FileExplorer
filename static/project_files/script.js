4e45aea6e242fa5e6de3d8b94de74c01ef090e4d
I-am-aman <readyaman@gmail.com>
2020-05-19 11:53:22 +0530

    Sorting by size and time functionality added and bugs fixed

--- static/project_files/script.js
+++ static/project_files/script.js
@@ -17,17 +17,27 @@ childrenArray.forEach(element => {
 
 const sortStatus = {
     name: 'none',
-    // size:,
-    // time:
+    size: 'none',
+    time: 'none'
 }
 
-const sort_name = (items, option) => {
+const sort = (items, option, type) => {
     items.sort((item1, item2) => {
-        const name1 = item1.name.toUpperCase();
-        const name2 = item2.name.toUpperCase();
-        if(name1 < name2)
+        let value1, value2;
+        if(type === 'name'){
+            value1 = item1.name.toUpperCase();
+            value2 = item2.name.toUpperCase();
+        }else if(type === 'size'){
+            value1 = item1.size;
+            value2 = item2.size;
+        }else{
+            value1 = item1.time;
+            value2 = item2.time;
+        }
+
+        if(value1 < value2)
             return -1;
-        else if(name1 > name2)
+        else if(value1 > value2)
             return 1;
         return 0;
     });
@@ -44,21 +54,20 @@ document.getElementById('table_head_row').addEventListener('click', event => {
     if(event.target){
         $('ion-icon').remove();
 
-        if(event.target.id === 'name'){
-            if(['none', 'down'].includes(sortStatus.name)){
-                sort_name(items, 'up');
-                sortStatus.name = 'up';
+        if(['none', 'down'].includes(sortStatus[event.target.id])){
+            sort(items, 'up', event.target.id);
+            sortStatus[event.target.id] = 'up';
 
-                event.target.innerHTML += ' <ion-icon name="caret-up-circle"></ion-icon>';
-            }
-            else if(sortStatus.name === 'up'){
-                sort_name(items, 'down');
-                sortStatus.name = 'down';
-
-                event.target.innerHTML += ' <ion-icon name="caret-down-circle"></ion-icon>';
-            }
-        
-            fill_table_body(items);
+            event.target.innerHTML += ' <ion-icon name="caret-up-circle"></ion-icon>';
         }
+        else if(sortStatus[event.target.id] === 'up'){
+            sort(items, 'down', event.target.id);
+            sortStatus[event.target.id] = 'down';
+
+            event.target.innerHTML += ' <ion-icon name="caret-down-circle"></ion-icon>';
+        }
+    
+        fill_table_body(items);
+        
     }
 });
